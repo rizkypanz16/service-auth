d6260877facaaaa0142a9a092b4f25a3ff18468c
rizky panji <rizkypanji@example.com>
2023-02-12 21:45:27 +0700

    update env again 2145 12022023

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 var filename = " File DB Connection";
 dbdata = {
-  host: process.env.DBHOST,
-  user: process.env.DBUSER,
-  password: process.env.DBPASSWORD,
-  database: process.env.DBNAME
+  host: process.env.AUTH_DBHOST,
+  user: process.env.AUTH_DBUSER,
+  password: process.env.AUTH_DBPASSWORD,
+  database: process.env.AUTH_DBNAME
 }
 var connection = mysql.createConnection(dbdata);
 connection.connect(function(err){
